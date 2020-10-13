9219724fcc5ee71b57f9947d73dc5a454d39d24f
Leonardo Almeida de Araújo <leonardo.a@acad.ifma.edu.br>
2020-10-13 13:03:45 -0300

    Pacotes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import ProductAddEdit from './components/ProductAddEdit';
-import ProductDetails from './components/ProductDetails';
-import ProductList from './components/ProductList';
-import Product from './entity/Product';
+import React from "react";
+import "./App.css";
+import firebase from "./firebase";
+import { SpellInput } from "./SpellInput";
 
-const prodsList=[
-  new Product({id:1, name: 'Prod1', price:125, quantity:10}),
-  new Product({id:2, name: 'Prod2', price:114.99, quantity:3}),
-  new Product({id:3, name: 'Prod3', price:50, quantity:12}),
-  new Product({id:4, name: 'Prod4', price:15.50, quantity:247}),
-]
+function App() {
+  const [spells, setSpells] = React.useState([]);
+  const [newSpellName, setNewSpellName] = React.useState();
 
-const App =()=>{
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const db = firebase.firestore();
+      const data = await db.collection("spells").get();
+      setSpells(data.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+    };
+    fetchData();
+  }, []);
+
+  const onCreate = () => {
+    const db = firebase.firestore();
+    db.collection("spells").add({ name: newSpellName });
+  };
+
   return (
-    <Router>
-      <div className='App'>
-        <Navbar />
-          <div className='container'>
-            <Switch>
-
-              <Route exact path='/' component={Home} />
-
-              <Route exact path='/add' component={ProductAddEdit} />
-
-              <Route exact path='/details' component={ProductDetails} />
-
-              <Route
-                exact path='/list'
-                render={(props)=><ProductList prods={prodsList}/>}
-              />
-
-            </Switch>
-         </div>
-      </div>
-    </Router>
-  )
+    <ul>
+      <input
+        value={newSpellName}
+        onChange={e => setNewSpellName(e.target.value)}
+      />
+      <button onClick={onCreate}>Creat</button>
+      {spells.map(spell => (
+        <li key={spell.name}>
+          <SpellInput spell={spell} />
+        </li>
+      ))}
+    </ul>
+  );
 }
+
 export default App;
